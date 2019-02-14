9caebd3bc6c6a4473e3624ae782d964a42771f21
Sifan <todguo86@gmail.com>
2019-02-14 09:39:07 -0600

    Adding file to get factors of a number

---
+++ Sifan_get_factors.py
@@ -0,0 +1,26 @@
+def get_factors(p):
+    """input is a number.
+    output is a dict showing the primes and their powers"""
+    # using a dict to store the powers of every prime number
+    num_counts = {}
+    num_counts[2] = 0
+    while p % 2 == 0:
+        num_counts[2] += 1
+        p = p / 2
+
+    # loop the quotient until it reaches p
+    current_quotient = 3
+    while  current_quotient <= p:
+        while p % current_quotient == 0:
+            if current_quotient not in num_counts:
+                num_counts[current_quotient] = 1
+            else:
+                num_counts[current_quotient] += 1
+            p = p / current_quotient
+        current_quotient += 2
+    # print(num_counts)
+    return num_counts
+
+
+test_num = int(input())
+print(get_factors(test_num))
